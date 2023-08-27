533d5e9be8723c3b3c34c8bfa93d46b39c206510
Jaya Satish <jayasatish38@gmail.com>
2023-08-27 17:47:48 +0530

    Enhanced Python scripts

--- post_extra_script.py
+++ post_extra_script.py
@@ -22,13 +22,21 @@ def copy_files():
     ]
     for src, dest in files:
         shutil.copyfile(src, f'release/{dest}')
-    print("==========> Bin files are copied to the /release folder.")
+    print("==========> Bin files are copied to the /release folder.\n")
 
 
 def convert_single_bin():
+    build_dir = env.subst("$BUILD_DIR")
+    binary_path = os.path.join(build_dir, "firmware.bin")
+    bootloader_path = os.path.join(build_dir, "bootloader.bin")
+    partitions_path = os.path.join(build_dir, "partitions.bin")
+    release_path = os.path.join(env.subst("$PROJECT_DIR"), "release")
+    merged_file = os.path.join(release_path, f"esp32nat_Router+_full_v{version}_0x0.bin")
+    esptool_dir = env.PioPlatform().get_package_dir("tool-esptoolpy")
+    esptool_command = f'python {str(esptool_dir)}\\esptool.py --chip esp32 merge_bin -o {merged_file} --flash_freq 40m --flash_size 4MB 0x1000 {bootloader_path} 0x8000 {partitions_path} 0x10000 {binary_path}'
+    
     os.makedirs("release", exist_ok=True)
-    os.system(
-        f'esptool.py --chip esp32 merge_bin -o release/esp32nat_Router+_full_v{version}_0x0.bin --flash_freq 40m --flash_size 4MB 0x1000 release/bootloader.bin 0x10000 release/esp32nat_Router+_v{version}.bin 0x8000 release/partitions.bin')
+    env.Execute(esptool_command)
     zip_all_bins()
 
 
@@ -47,7 +55,7 @@ def zip_all_bins():
         zipObj.write(
             f'release/esp32nat_Router+_full_v{version}_0x0.bin', f'esp32nat_Router+_full_v{version}_0x0.bin')
 
-    print("==========> zip files created successfully")
+    print("\n==========> zip files created successfully")
 
 
 def after_build_program(source, target, env):
@@ -57,4 +65,4 @@ def after_build_program(source, target, env):
         convert_single_bin()
 
 
-env.AddPostAction("$BUILD_DIR/firmware.bin", after_build_program)
+env.AddPostAction("buildprog", after_build_program)
